8ab8bff0f1240ee645f756c9e497859c8d9bdd2c
Ubuntu <ubuntu@ip-172-31-91-139.ec2.internal>
2023-11-03 13:36:24 +0000

    Empezamos practica de WordPress

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # practica-01-06
+Nueva practica donde practicaremos WordPress
+
+# Carpeta Scripts
---
+++ scripts/install_lamp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Muestra todos los comandos que se van ejecutadno
+set -ex
+
+# Actualizamos los repositorios
+apt update
+
+# actualizamos los paquetes 
+#apt upgrade -y
+
+#instalamos el servidor web Apache
+apt install apache2 -y
+
+
+#Instalar  el sistema gestor de datos MySQL
+apt install mysql-server -y
+
+#DB_USER=usuario
+#DB_PASSWD=contraseña
+
+#mysql -u $DB_USER -p$DB_PASSWD < ../sql/database.sql
+
+#Instalamos php
+sudo apt install php libapache2-mod-php php-mysql -y
+
+#Reiniciamos servicio
+systemctl restart apache2
+
+# Modificamos el propietario
+chown -R www-data:www-data /var/www/html
